511f76a8c1883ebdd5cef2e48e5c761cdd6e6d3d
yacinegithub <35470755+yacinegithub@users.noreply.github.com>
2024-04-09 09:25:48 +0100

    Update [...all].vue

--- frontend/pages/[...all].vue
+++ frontend/pages/[...all].vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   useHead({
-    title: "404. Not Found",
+    title: "404. صفحة غير موجودة",
   });
   definePageMeta({
     layout: "404",
@@ -10,6 +10,6 @@
 <template>
   <h1 class="text-blue-500 font-extrabold flex flex-col text-center">
     <span class="text-7xl">404.</span>
-    <span class="text-5xl mt-5"> Page Not Found </span>
+    <span class="text-5xl mt-5"> صفحة غير موجودة </span>
   </h1>
 </template>
